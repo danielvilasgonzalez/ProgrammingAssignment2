95128fcf9c30743423930bd0db9a176425f7101b
Daniel Vilas Gonzalez <danielvilasgonzalez@gmail.com>
2015-03-19 23:33:13 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+#My matrix
 
-## Write a short comment describing this function
+## x = matrix which returns a list, containing a function to
+
+###1.-set the value of the vector
+###2.-get the value of the vector
+###3.-set the value of the mean
+###4.-get the value of the mean  
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m<-NULL
+  set<-function(y) {
+## assigning a value to an object in a different environment
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setInv<-function(inv) m <<-inv
+  getInv<-function() m
+  list(set=set,get=get,
+       setInv=setInv,
+       getInv=getInv)
+  
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+## inverse of the original matrix -------- makeCacheMatrix()
+  
+  m<-x$getInv()
+  if (!is.null(m)) {
+    print
+    return(m)
+    
+## returning the inverted matrix
+
+  }
+  m<-solve(x$get())
+  x$setInv(m)
+  m
 }
